23652d01be0ebb5fc740ae30772ddb41d23238df
VanoAkofashvili <vanikoakofa@gmail.com>
2020-10-27 23:35:03 +0400

    player can input name

--- script.js
+++ script.js
@@ -17,12 +17,15 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const btnRules = document.querySelector('.btn--rules');
 const btnClose = document.querySelector('.btn--close');
+const btnInput = document.querySelector('.btn--input');
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let state;
+let player1;
+let player2;
 
 const init = function () {
   state = {
@@ -31,6 +34,7 @@ const init = function () {
     playing: true,
     scores: [0, 0],
     modalOpened: false,
+    firstLoad: true,
   };
 
   score0El.textContent = 0;
@@ -48,7 +52,45 @@ const init = function () {
   player0El.classList.add('player--active');
 };
 
+// Modal
+const openModal = function () {
+  modalEl.classList.remove('hidden-popup');
+  overlay.classList.remove('hidden-popup');
+};
+
+const closeModal = function () {
+  modalEl.classList.add('hidden-popup');
+  if (!state.firstLoad) {
+    overlay.classList.add('hidden-popup');
+  }
+};
+
+btnRules.addEventListener('click', function () {
+  if (!state.modal) {
+    openModal();
+  }
+});
+
+const inputNames = function () {
+  document.getElementById('name_inputs').classList.remove('hidden-popup');
+  overlay.classList.remove('hidden-popup');
+};
+
+inputNames();
 init();
+
+btnInput.addEventListener('click', function () {
+  player1 = document.getElementById('plr1').value;
+  player2 = document.getElementById('plr2').value;
+
+  if (player1 && player2 && player1.length < 15 && player2.length <= 15) {
+    document.getElementById('name--0').textContent = player1;
+    document.getElementById('name--1').textContent = player2;
+    state.firstLoad = false;
+    closeModal();
+    document.getElementById('name_inputs').classList.add('hidden-popup');
+  }
+});
 
 const switchPlayer = function () {
   document.getElementById(`current--${state.activePlayer}`).textContent = 0;
@@ -87,9 +129,8 @@ btnHold.addEventListener('click', function () {
       document
         .querySelector(`.player--${state.activePlayer}`)
         .classList.add('player--winner');
-      document
-        .getElementById(`name--${state.activePlayer}`)
-        .textContent = 'Winner';
+      document.getElementById(`name--${state.activePlayer}`).textContent =
+        'Winner';
       document
         .querySelector(`.player--${state.activePlayer}`)
         .classList.remove('player--active');
@@ -100,23 +141,6 @@ btnHold.addEventListener('click', function () {
 });
 
 btnNew.addEventListener('click', init);
-
-// Modal
-const openModal = function () {
-  modalEl.classList.remove('hidden-popup');
-  overlay.classList.remove('hidden-popup');
-};
-
-const closeModal = function () {
-  modalEl.classList.add('hidden-popup');
-  overlay.classList.add('hidden-popup');
-};
-
-btnRules.addEventListener('click', function () {
-  if (!state.modal) {
-    openModal();
-  }
-});
 
 overlay.addEventListener('click', closeModal);
 
